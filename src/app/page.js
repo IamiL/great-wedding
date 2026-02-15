dcd7035d875bdc9e02af0e06721615eae7bf4673
Иван Лебедев <iamil50113@gmail.com>
2026-02-15 05:32:55 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -152,6 +152,17 @@ export default function Home() {
                         <br/>
                         Магистральная, 54
                     </p>
+                    <a
+                        href="https://yandex.ru/maps/?text=Село Большая Ельня, Магистральная, 54"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.mapButton}
+                    >
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="currentColor"/>
+                        </svg>
+                        Открыть в Яндекс.Картах
+                    </a>
                 </div>
                 <div id={styles.dateTimeBackground}>
                     {/* Старый фон с фото заката */}
